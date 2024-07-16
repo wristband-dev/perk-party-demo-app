9ca5d5185bc80c23c660658daaed064ee105c2fe
Donato DiFerdinando <fddiferd@gmail.com>
2024-07-16 08:15:34 -0700

    - modal working

--- src/components/PerkCard.jsx
+++ src/components/PerkCard.jsx
@@ -2,40 +2,77 @@ import { useState } from 'react';
 
 export function PerkCard({ image, perkName, perkDesc, banner }) {
   const [isHovered, setIsHovered] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
-    <div 
-      className="w-[320px] h-[250px] mb-16 mx-8 bg-black shadow-2xl border-gray-200 rounded-lg overflow-hidden relative"
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      <div className="h-3/5">
-        <img src={image} alt="Logo" className="w-full h-full object-cover cursor-pointer" />
-        {banner && (
-          <div className="absolute top-0 right-0 p-2">
-            <h1 className="text-xl pt-2 text-white bg-pink-600 rounded-lg font-bold p-2">{banner}</h1>
+    <>
+      <div 
+        className="w-[320px] h-[250px] mb-16 mx-8 bg-black shadow-2xl border-gray-200 rounded-lg overflow-hidden relative"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        onClick={() => setIsModalOpen(true)}
+      >
+        <div className="h-3/5">
+          <img src={image} alt="Logo" className="w-full h-full object-cover cursor-pointer" />
+          {banner && (
+            <div className="absolute top-0 right-0 p-2">
+              <h1 className="text-xl pt-2 text-white bg-pink-600 rounded-lg font-bold p-2">{banner}</h1>
+            </div>
+          )}
+        </div>
+        <div className="p-4 h-2/5 flex flex-col justify-between">
+          <h1 
+            className={`text-xl text-white font-bold ${perkName.length > 20 ? 'text-lg' : ''}`}
+            style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}
+          >
+            {perkName}
+          </h1>
+          <p 
+            className="text-sm text-white overflow-hidden"
+            style={{ display: '-webkit-box', WebkitLineClamp: 2, WebkitBoxOrient: 'vertical' }}
+          >
+            {perkDesc}
+          </p>
+        </div>
+        {isHovered && (
+          <div className="absolute inset-0 text-white bg-black bg-opacity-90 p-4 flex items-center justify-center">
+            <p className="text-lg font-bold italic">{perkDesc}</p>
           </div>
         )}
       </div>
-      <div className="p-4 h-2/5 flex flex-col justify-between">
-        <h1 
-          className={`text-xl text-white font-bold ${perkName.length > 20 ? 'text-lg' : ''}`}
-          style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}
-        >
-          {perkName}
-        </h1>
-        <p 
-          className="text-sm text-white overflow-hidden"
-          style={{ display: '-webkit-box', WebkitLineClamp: 2, WebkitBoxOrient: 'vertical' }}
-        >
-          {perkDesc}
-        </p>
-      </div>
-      {isHovered && (
-        <div className="absolute inset-0 text-white bg-black bg-opacity-90 p-4 flex items-center justify-center">
-          <p className="text-lg font-bold italic">{perkDesc}</p>
+
+      {isModalOpen && (
+        <div className="p-10 fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-75">
+          <div className="bg-white rounded-lg shadow-lg relative">
+            <section className="m-0 relative">
+              <img 
+                src={image}
+                alt="Logo" 
+                className="max-h-[400px] w-full object-cover rounded-lg"
+              />
+              <button 
+                type="button" 
+                className="absolute top-0 right-0 m-4 bg-black text-white hover:bg-pink-600 border border-black focus:ring-4 focus:outline-none focus:ring-pink-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" 
+                onClick={() => setIsModalOpen(false)}
+              >
+                &times;
+              </button>
+            </section>
+            <div className="flex flex-col m-4">
+              <h1 className="text-2xl font-bold mb-2">{perkName}</h1>
+              <p className="text-lg">{perkDesc}</p>
+            </div>
+            <div className="flex justify-center mb-4">
+              <button 
+                type="button" 
+                className="bg-black text-white hover:bg-pink-600 border border-black focus:ring-4 focus:outline-none focus:ring-pink-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+              >
+                Claim
+              </button>
+            </div>
+          </div>
         </div>
       )}
-    </div>
+    </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { apiClient as axios } from '@/client/browser-axios-client';
-
+import { useState } from 'react';
 import { useAuth } from '@/context/auth-context';
 import { Raleway } from "next/font/google";
 import { PerkCard } from '@/components/PerkCard';
@@ -11,6 +11,7 @@ const perks = [
     id: 1,
     image: 'https://cdn.pixabay.com/photo/2022/11/29/08/54/race-car-7624025_1280.jpg',
     perkName: 'Race Rental Car',
+    category: 'Thrill',
     perkDesc: 'Experience the thrill of driving a race car for a day! Speed through the track and feel the adrenaline. Perfect for both seasoned racers and beginners. Your dream ride awaits!',
     banner: 'NEW'
   },
@@ -18,6 +19,7 @@ const perks = [
     id: 2,
     image: 'https://images.unsplash.com/photo-1501147830916-ce44a6359892?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YmlrZXxlbnwwfHwwfHx8MA%3D%3D',
     perkName: 'Bike Pass',
+    category: 'Travel',
     perkDesc: 'Enjoy unlimited bike rides throughout the city with our exclusive Bike Pass. Stay active and explore your surroundings effortlessly.',
     banner: 'TRENDING'
   },
@@ -25,6 +27,7 @@ const perks = [
     id: 3,
     image: 'https://images.unsplash.com/uploads/1413387158190559d80f7/6108b580?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     perkName: 'Rail Pass',
+    category: 'Travel',
     perkDesc: 'Travel conveniently across the region with our comprehensive Rail Pass, providing access to all major rail lines.',
     banner: 'TRENDING'
   },
@@ -32,6 +35,7 @@ const perks = [
     id: 4,
     image: 'https://images.unsplash.com/photo-1556695736-d287caebc48e?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWV0cm98ZW58MHx8MHx8fDA%3D',
     perkName: 'Metro Pass',
+    category: 'Travel',
     perkDesc: 'Unlimited metro rides for a month, ensuring seamless and affordable city travel for your daily commute.',
     banner: ''
   },
@@ -39,6 +43,7 @@ const perks = [
     id: 5,
     image: 'https://images.unsplash.com/photo-1544367567-0f2fcb009e0b?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8eW9nYXxlbnwwfHwwfHx8MA%3D%3D',
     perkName: '30 Minute Hot Yoga Coupons',
+    category: 'Relax',
     perkDesc: 'Relax and rejuvenate with 30-minute hot yoga sessions. Perfect for unwinding after a long day.',
     banner: ''
   },
@@ -46,6 +51,7 @@ const perks = [
     id: 6,
     image: 'https://plus.unsplash.com/premium_photo-1669374537810-f88d8ad82818?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Y29mZmVlfGVufDB8fDB8fHww',
     perkName: 'Blue Bottle Coffee Delivered Intravenously',
+    category: 'Relax',
     perkDesc: 'Get your caffeine fix instantly with our Blue Bottle Coffee IV delivery service. Perfect for those who need an extra boost.',
     banner: ''
   },
@@ -60,6 +66,7 @@ const perks = [
     id: 8,
     image: 'https://images.unsplash.com/photo-1457732815361-daa98277e9c8?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c2NyZWFtJTIwcm9vbXxlbnwwfHwwfHx8MA%3D%3D',
     perkName: '1-Hour Scream Room Coupons',
+    category: 'Thrill',
     perkDesc: 'Release your frustrations in our soundproof Scream Room for an hour. Scream away stress and feel refreshed!',
     banner: ''
   },
@@ -67,6 +74,7 @@ const perks = [
     id: 9,
     image: 'https://plus.unsplash.com/premium_photo-1676901918792-f6e04df8b7b4?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aGFtbWVyfGVufDB8fDB8fHww',
     perkName: '1-Hour Rage Room Tokens',
+    category: 'Thrill',
     perkDesc: 'Smash away your stress in our Rage Room. Enjoy an hour of therapeutic destruction.',
     banner: ''
   },
@@ -74,6 +82,7 @@ const perks = [
     id: 10,
     image: 'https://images.unsplash.com/photo-1540946485063-a40da27545f8?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2FpbGJvYXR8ZW58MHx8MHx8fDA%3D',
     perkName: 'Free Sailing Lessons',
+    category: 'Travel',
     perkDesc: 'Learn the basics of sailing with our free lessons. Perfect for aspiring sailors and water enthusiasts.',
     banner: ''
   },
@@ -81,6 +90,7 @@ const perks = [
     id: 11,
     image: 'https://images.unsplash.com/photo-1565109254792-8856bba5abfb?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8c2Nvb3RlcnxlbnwwfHwwfHx8MA%3D%3D',
     perkName: 'E-Scooters for Remote Workers',
+    category: 'Travel',
     perkDesc: 'Refuse to work in the office? No problem! Enjoy free e-scooter rides to get around the city.',
     banner: ''
   },
@@ -88,6 +98,7 @@ const perks = [
     id: 12,
     image: 'https://images.unsplash.com/photo-1504708706948-13d6cbba4062?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8R3Jhbm9sYXxlbnwwfHwwfHx8MA%3D%3D',
     perkName: 'Granola',
+    category: 'Food',
     perkDesc: 'Keep your energy up with our delicious and nutritious granola packs. Perfect for snacking on the go.',
     banner: ''
   },
@@ -95,6 +106,7 @@ const perks = [
     id: 13,
     image: 'https://images.unsplash.com/photo-1577344718665-3e7c0c1ecf6b?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bWVkaXRhdGlvbnxlbnwwfHwwfHx8MA%3D%3D',
     perkName: 'Virtual Reality Meditation',
+    category: 'Relax',
     perkDesc: 'Experience tranquility with our VR meditation sessions. Escape to peaceful virtual landscapes and relax.',
     banner: ''
   },
@@ -102,6 +114,7 @@ const perks = [
     id: 14,
     image: 'https://images.unsplash.com/photo-1577106263724-2c8e03bfe9cf?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2hlZnxlbnwwfHwwfHx8MA%3D%3D',
     perkName: 'Personal Chef Service',
+    category: 'Food',
     perkDesc: 'Enjoy gourmet meals prepared by a personal chef in the comfort of your home. Perfect for special occasions or daily indulgence.',
     banner: ''
   },
@@ -109,6 +122,7 @@ const perks = [
       id: 15,
       image: 'https://images.unsplash.com/photo-1600334089648-b0d9d3028eb2?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c3BhfGVufDB8fDB8fHww',
       perkName: 'Spa Day Pass',
+      category: 'Relax',
       perkDesc: 'Relax and rejuvenate with a full day pass to our luxurious spa. Enjoy massages, facials, and more.',
       banner: ''
   },
@@ -116,6 +130,7 @@ const perks = [
       id: 16,
       image: 'https://images.unsplash.com/photo-1536440136628-849c177e76a1?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2luZW1hfGVufDB8fDB8fHww',
       perkName: 'Private Cinema Screening',
+      category: 'Relax',
       perkDesc: 'Watch your favorite movies in a private cinema setting. Enjoy a personalized viewing experience.',
       banner: ''
   },
@@ -123,6 +138,7 @@ const perks = [
       id: 17,
       image: 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGNvb2tpbmd8ZW58MHx8MHx8fDA%3D',
       perkName: 'Gourmet Cooking Classes',
+      category: 'Food',
       perkDesc: 'Learn to cook like a pro with our gourmet cooking classes. Perfect for food enthusiasts and aspiring chefs.',
       banner: ''
   },
@@ -130,6 +146,7 @@ const perks = [
       id: 18,
       image: 'https://images.unsplash.com/photo-1470229722913-7c0e2dbbafd3?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y29uY2VydHxlbnwwfHwwfHx8MA%3D%3D',
       perkName: 'Exclusive Concert Tickets',
+      category: 'Relax',
       perkDesc: 'Get front-row tickets to exclusive concerts and enjoy live performances from your favorite artists.',
       banner: ''
   },
@@ -137,6 +154,7 @@ const perks = [
       id: 19,
       image: 'https://plus.unsplash.com/premium_photo-1674852890869-045efbe8c54b?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8d29vZHdvcmtpbmd8ZW58MHx8MHx8fDA%3D',
       perkName: 'Artisanal Craft Workshops',
+      category: 'Thrill',
       perkDesc: 'Unleash your creativity with our artisanal craft workshops. Learn new skills and create beautiful handmade items.',
       banner: ''
   }
@@ -166,144 +184,20 @@ export default function HomePage() {
       <option value="thrill">Thrill</option>
       <option value="relax">Relax</option>
       <option value="travel">Travel</option>
+      <option value="travel">Food</option>
     </select>
   </div>
 </div>
     </div>
         <div className='mt-4 mx-4 flex flex-wrap justify-center z-10'>
-          <PerkCard 
-              image='https://cdn.pixabay.com/photo/2022/11/29/08/54/race-car-7624025_1280.jpg'
-              perkName='Race Rental Car'
-              perkDesc='Experience the thrill of driving a race car for a day! Speed through the track and feel the adrenaline. Perfect for both seasoned racers and beginners. Your dream ride awaits!'
-              banner='NEW'
-          />
-          
-          <PerkCard 
-              image='https://images.unsplash.com/photo-1501147830916-ce44a6359892?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YmlrZXxlbnwwfHwwfHx8MA%3D%3D'
-              perkName='Bike Pass'
-              perkDesc='Enjoy unlimited bike rides throughout the city with our exclusive Bike Pass. Stay active and explore your surroundings effortlessly.'
-              banner='TRENDING'
-          />
-
-          <PerkCard 
-              image="https://images.unsplash.com/uploads/1413387158190559d80f7/6108b580?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
-              perkName='Rail Pass'
-              perkDesc='Travel conveniently across the region with our comprehensive Rail Pass, providing access to all major rail lines.'
-              banner='TRENDING'
-          />
-
-          <PerkCard 
-              image='https://images.unsplash.com/photo-1556695736-d287caebc48e?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWV0cm98ZW58MHx8MHx8fDA%3D'
-              perkName='Metro Pass'
-              perkDesc='Unlimited metro rides for a month, ensuring seamless and affordable city travel for your daily commute.'
-              banner=''
-          />
-
-          <PerkCard 
-              image='https://images.unsplash.com/photo-1544367567-0f2fcb009e0b?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8eW9nYXxlbnwwfHwwfHx8MA%3D%3D'
-              perkName='30 Minute Hot Yoga Coupons'
-              perkDesc='Relax and rejuvenate with 30-minute hot yoga sessions. Perfect for unwinding after a long day.'
-              banner=''
-          />
-
-          <PerkCard 
-              image='https://plus.unsplash.com/premium_photo-1669374537810-f88d8ad82818?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Y29mZmVlfGVufDB8fDB8fHww'
-              perkName='Blue Bottle Coffee Delivered Intravenously'
-              perkDesc='Get your caffeine fix instantly with our Blue Bottle Coffee IV delivery service. Perfect for those who need an extra boost.'
-              banner=''
-          />
-
-          <PerkCard 
-              image='https://images.unsplash.com/photo-1546527868-ccb7ee7dfa6a?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHVwcHl8ZW58MHx8MHx8fDA%3D'
-              perkName='Puppy Hour'
-              perkDesc='Cuddle with adorable puppies every Wednesday during our Puppy Hour. A perfect stress reliever!'
-              banner=''
-          />
-
-          <PerkCard 
-              image='https://images.unsplash.com/photo-1457732815361-daa98277e9c8?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c2NyZWFtJTIwcm9vbXxlbnwwfHwwfHx8MA%3D%3D' 
-              perkName='1-Hour Scream Room Coupons'
-              perkDesc='Release your frustrations in our soundproof Scream Room for an hour. Scream away stress and feel refreshed!'
-              banner=''
-          />
-
-          <PerkCard 
-              image='https://plus.unsplash.com/premium_photo-1676901918792-f6e04df8b7b4?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aGFtbWVyfGVufDB8fDB8fHww'
-              perkName='1-Hour Rage Room Tokens'
-              perkDesc='Smash away your stress in our Rage Room. Enjoy an hour of therapeutic destruction.'
-              banner=''
-          />
-
-          <PerkCard 
-              image='https://images.unsplash.com/photo-1540946485063-a40da27545f8?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2FpbGJvYXR8ZW58MHx8MHx8fDA%3D'
-              perkName='Free Sailing Lessons'
-              perkDesc='Learn the basics of sailing with our free lessons. Perfect for aspiring sailors and water enthusiasts.'
-              banner=''
-          />
-
-          <PerkCard 
-              image='https://images.unsplash.com/photo-1565109254792-8856bba5abfb?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8c2Nvb3RlcnxlbnwwfHwwfHx8MA%3D%3D'
-              perkName='E-Scooters for Remote Workers'
-              perkDesc='Refuse to work in the office? No problem! Enjoy free e-scooter rides to get around the city.'
-              banner=''
-          />
-
-          <PerkCard 
-              image='https://images.unsplash.com/photo-1504708706948-13d6cbba4062?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8R3Jhbm9sYXxlbnwwfHwwfHx8MA%3D%3D'
-              perkName='Granola'
-              perkDesc='Keep your energy up with our delicious and nutritious granola packs. Perfect for snacking on the go.'
-              banner=''
-          />
-
-          <PerkCard 
-              image='https://images.unsplash.com/photo-1577344718665-3e7c0c1ecf6b?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bWVkaXRhdGlvbnxlbnwwfHwwfHx8MA%3D%3D'
-              perkName='Virtual Reality Meditation'
-              perkDesc='Experience tranquility with our VR meditation sessions. Escape to peaceful virtual landscapes and relax.'
-              banner=''
-          />
-
-          <PerkCard 
-              image='https://images.unsplash.com/photo-1577106263724-2c8e03bfe9cf?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2hlZnxlbnwwfHwwfHx8MA%3D%3D'
-              perkName='Personal Chef Service'
-              perkDesc='Enjoy gourmet meals prepared by a personal chef in the comfort of your home. Perfect for special occasions or daily indulgence.'
-              banner=''
-          />
-
-          <PerkCard 
-              image='https://images.unsplash.com/photo-1600334089648-b0d9d3028eb2?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c3BhfGVufDB8fDB8fHww'
-              perkName='Spa Day Pass'
-              perkDesc='Relax and rejuvenate with a full day pass to our luxurious spa. Enjoy massages, facials, and more.'
-              banner=''
-          />
-
-          <PerkCard 
-              image='https://images.unsplash.com/photo-1536440136628-849c177e76a1?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2luZW1hfGVufDB8fDB8fHww'
-              perkName='Private Cinema Screening'
-              perkDesc='Watch your favorite movies in a private cinema setting. Enjoy a personalized viewing experience.'
-              banner=''
-          />
-
-          <PerkCard 
-              image='https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGNvb2tpbmd8ZW58MHx8MHx8fDA%3D'
-              perkName='Gourmet Cooking Classes'
-              perkDesc='Learn to cook like a pro with our gourmet cooking classes. Perfect for food enthusiasts and aspiring chefs.'
-              banner=''
-          />
-
-          <PerkCard 
-              image='https://images.unsplash.com/photo-1470229722913-7c0e2dbbafd3?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y29uY2VydHxlbnwwfHwwfHx8MA%3D%3D'
-              perkName='Exclusive Concert Tickets'
-              perkDesc='Get front-row tickets to exclusive concerts and enjoy live performances from your favorite artists.'
-              banner=''
-          />
-
-          <PerkCard 
-              image='https://plus.unsplash.com/premium_photo-1674852890869-045efbe8c54b?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8d29vZHdvcmtpbmd8ZW58MHx8MHx8fDA%3D'
-              perkName='Artisanal Craft Workshops'
-              perkDesc='Unleash your creativity with our artisanal craft workshops. Learn new skills and create beautiful handmade items.'
-              banner=''
-          />
-
+          {perks.map(perk => (
+            <PerkCard 
+              image={perk.image}
+              perkName={perk.perkName}
+              perkDesc={perk.perkDesc}
+              banner={perk.banner}
+            />
+          ))}
         </div>
       </section>
     </>
