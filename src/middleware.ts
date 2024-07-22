d930f870b3dec0414d6430157ce06c5ab9fa8f43
Jim Verducci <jim@wristband.dev>
2024-07-22 13:04:16 -0700

    Add debug logs for Vercel to find bug

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,6 +8,7 @@ export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
   const host = req.headers.get('host');
   const { pathname } = req.nextUrl;
+  console.log(`\nHOST: [${host}], PATHNAME: [${pathname}]\n`);
 
   // Path matching here is crude -- replace with whatever matching algorithm your app needs.
   const isProtectedPage: boolean = ['/settings', '/admin'].includes(pathname);
@@ -15,6 +16,7 @@ export async function middleware(req: NextRequest) {
 
   // Simply return if the path is not meant to be protected
   if (!isProtectedPage && !isProtectedApiRoute) {
+    console.log(`\nIS NOT A PROTECTED PAGE OR ROUTE!!!\n`);
     return res;
   }
 
@@ -23,9 +25,11 @@ export async function middleware(req: NextRequest) {
 
   const returnUrl = `${PERK_PARTY_PROTOCOL}://${host}${pathname}`;
   const loginUrl = `${PERK_PARTY_PROTOCOL}://${host}/api/auth/login?return_url=${returnUrl}`;
+  console.log(`\LOGIN_URL: [${loginUrl}]\n`);
 
   // Send users to the login page if they attempt to access protected paths when unauthenticated.
   if (!isAuthenticated) {
+    console.log(`\nHOST: [${host}], PATHNAME: [${pathname}]\n`);
     return isProtectedApiRoute ? NextResponse.json(UNAUTHORIZED, HTTP_401_STATUS) : NextResponse.redirect(loginUrl);
   }
 
@@ -46,6 +50,7 @@ export async function middleware(req: NextRequest) {
     return isProtectedApiRoute ? NextResponse.json(UNAUTHORIZED, HTTP_401_STATUS) : NextResponse.redirect(loginUrl);
   }
 
+  console.log(`\nMIDDLEWARE SUCCESS!!!\n`);
   return res;
 }
 
--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -417,6 +417,7 @@ export const getServerSideProps: GetServerSideProps = async function (context: G
 
   /* WRISTBAND_TOUCHPOINT - AUTHENTICATION */
   if (!isAuthenticated) {
+    console.log(`\nSETTINGS_PAGE: SERVER REDIRECT TO LOGIN!!!\n`);
     return serverRedirectToLogin(req);
   }
 
