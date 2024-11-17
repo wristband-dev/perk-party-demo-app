22b3f68f8e14630ea28bdd4fb2cf2769479d7052
Jim Verducci <jim@wristband.dev>
2024-11-16 20:56:35 -0800

    Bump wristband auth version

--- src/client/frontend-api-client.ts
+++ src/client/frontend-api-client.ts
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const JSON_MEDIA_TYPE = 'application/json';
-const XSRF_COOKIE_NAME = 'XSRF-TOKEN';
-const XSRF_HEADER_NAME = 'X-XSRF-TOKEN';
+import { CSRF_TOKEN_COOKIE_NAME, CSRF_TOKEN_HEADER_NAME, JSON_MEDIA_TYPE } from '@/utils/constants';
 
 // Axios has XSRF token handling by default.  We still specify the values in the config
 // here merely to be explicit about which names are being used under the hood.
@@ -10,8 +8,8 @@ const defaultOptions = {
   // Set up baseURL based on whether this is server-side or client-side
   baseURL: typeof window !== 'undefined' ? `${window.location.origin}/api/v1` : undefined,
   headers: { 'Content-Type': JSON_MEDIA_TYPE, Accept: JSON_MEDIA_TYPE },
-  xsrfCookieName: XSRF_COOKIE_NAME,
-  xsrfHeaderName: XSRF_HEADER_NAME,
+  xsrfCookieName: CSRF_TOKEN_COOKIE_NAME,
+  xsrfHeaderName: CSRF_TOKEN_HEADER_NAME,
 };
 
 const frontendApiClient = axios.create(defaultOptions);
